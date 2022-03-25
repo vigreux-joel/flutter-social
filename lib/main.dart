93e1ac17fefdda4ea87f5ec7e5edeeb16f67bf9f
vigreux <vigreux.joel@hotmail.com>
2022-03-25 11:54:53 +0100

    0.0.1 example: messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const FriendlyChatApp(),
+  );
 }
 
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(          // Remove the const from here
-      title: 'Startup Name Generator',
-      theme: ThemeData(          // Add the 5 lines from here...
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.deepPurpleAccent,
-        ),
-      ),                         // ... to here.
-      home: const RandomWords(), // And add the const back here.
+    return const MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
     );
   }
 }
 
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  State<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <String>[];                 // NEW
-  final _biggerFont = const TextStyle(fontSize: 18); // NEW
-  final _saved = <String>{};     // NEW
-  // final _biggerFont = TextStyle(fontSize: 18);
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
-        // Add from here ...
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+              const InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
           ),
         ],
-        // ... to here
       ),
-      body: _buildSuggestions(),
     );
   }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      // Add lines from here...
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-                (pair) {
-              return ListTile(
-                title: Text(
-                  pair,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ), // ...to here.
-    );
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.add('mot n°'+(index+1).toString());
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-  Widget _buildRow(String word)
-  {
-    final alreadySaved = _saved.contains(word);  // NEW
-    return ListTile(
-      title: Text(
-        word,
-        style: _biggerFont,
-      ),
-      trailing: Icon(     // NEW from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-      ),
-      onTap: () {      // NEW lines from here...
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(word);
-          } else {
-            _saved.add(word);
-          }
-        });
-      },
-    );
-  }
-
-
-
-
 }
-
